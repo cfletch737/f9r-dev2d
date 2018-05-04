9319a944b6e3b1ff4998fc75a868e216ad9b0874
cfletch737 <34666268+cfletch737@users.noreply.github.com>
2018-05-04 12:14:33 -0400

    Update craft.js

--- assets/scripts/craft.js
+++ assets/scripts/craft.js
@@ -91,7 +91,7 @@ var Craft=function(options) {
       angle: radians(8),
       angular_velocity: radians(-0.3),
       rcs_fuel:300,
-      fuel: 4000,
+      fuel: 3500,
       gear_down:false,
       clamp:false,
       model:"f9r-dev-high"
